f8038878d26ff32ede166691b1b88de6eb5b10e2
Samuel Silva <samuelsilvawb@gmail.com>
2023-06-25 15:15:59 +0100

    style: setup eslint and prettier as linters

---
+++ .eslintrc.cjs
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    node: true,
+  },
+  parser: "@typescript-eslint/parser",
+  extends: ["eslint:recommended", "plugin:astro/recommended"],
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    sourceType: "module",
+    ecmaVersion: "latest",
+  },
+  overrides: [
+    {
+      files: ["*.astro"],
+      parser: "astro-eslint-parser",
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+      },
+      rules: {},
+    },
+  ],
+};
